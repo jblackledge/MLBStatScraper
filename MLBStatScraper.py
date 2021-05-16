825993459666103fa4750d97a9298fa29db5ee37
Jesse <jesse.m.blackledge-1@ou.edu>
2021-05-16 21:48:41 +0000

    Added another parameter to scraping function that takes in a the stat type string, named statType. Created dictionary within stat scraping function to allow get correct column value based on the statType string, and use the column string value to choose which section of html is parsed. This however breaks the current way of getting the stat value as the current method is based specifically on OPS. Further debugging due

--- MLBStatScraper.py
+++ MLBStatScraper.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup as soup
 from urllib.request import Request, urlopen
 
 
-def getOPSLeaders(league):
+def getOPSLeaders(league, statType):
+	statDictionary = {"OPS" : "17", "AVG" : "14", "HR" : "8", "RBI" : "9"}
 	#urls to parse, use request with "User-Agent" header to prevent MLB from blocking bot
 	urlOPS = "https://www.mlb.com/stats/" + league + "-league"
 	request = Request(urlOPS, headers={"User-Agent": "XYZ/3.0"})
@@ -20,7 +21,7 @@ def getOPSLeaders(league):
 	playerNames = opsSoup.find_all("span", {"class": "full-3fV3c9pF"})
 
 	#find column specific stat
-	stats = opsSoup.select("td[data-col=\"17\"]")
+	stats = opsSoup.select("td[data-col=\"" + statDictionary[statType]  + "\"]")
 
 
 	#loop through list of player name by 2, and parse the html to retrieve a str object for first and last name
@@ -55,4 +56,4 @@ def getOPSLeaders(league):
 		statIndex += 1
 
 
-getOPSLeaders("american")
+getOPSLeaders("national", "OPS")
