24684f7eedc91d1082b43a8b559cb71abf3a2a21
Jesse <jesse.m.blackledge-1@ou.edu>
2021-05-16 16:08:20 +0000

    Added comments

--- MLBWebScraper.py
+++ MLBWebScraper.py
@@ -2,13 +2,16 @@ import urllib.request
 from bs4 import BeautifulSoup as soup
 from urllib.request import Request, urlopen
 
-
+#urls to parse, use request with "User-Agent" header to prevent MLB from blocking bot
 urlOPS = "https://www.mlb.com/stats/national-league"
 request = Request(urlOPS, headers={"User-Agent": "XYZ/3.0"})
-downloadedOPSPage = urlopen(request)
 
+#download webpage, read contents, and close reader
+downloadedOPSPage = urlopen(request)
 opsPageHtml = downloadedOPSPage.read()
 downloadedOPSPage.close()
+
+#create Beautiful Soup object to parse html page contents
 opsSoup = soup(opsPageHtml, "html.parser")
 
 print(opsSoup.h1)
