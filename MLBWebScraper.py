75eca55a7763e55e864498a5710fdf4f8eb2dca1
Jesse <jesse.m.blackledge-1@ou.edu>
2021-05-16 19:30:23 +0000

    Added parsing of stat value, test found formatting and indexing errors. Debugging needed

--- MLBWebScraper.py
+++ MLBWebScraper.py
@@ -17,6 +17,10 @@ opsnlSoup = soup(opsnlPageHtml, "html.parser")
 #find place in HTML containing player names in the table (ordered based on selected attribute)
 playerNames = opsnlSoup.find_all("span", {"class": "full-3fV3c9pF"})
 
+#find column specific stat
+stats = opsnlSoup.select("td[data-col=\"17\"]")
+
+
 #loop through list of player name by 2, and parse the html to retrieve a str object for first and last name
 rank = 1;
 for i in range(0, len(playerNames), 2):
@@ -27,7 +31,11 @@ for i in range(0, len(playerNames), 2):
 	#parse string, leaving only the name
 	firstName = str(firstNameLine[28 : -7])
 	lastName = str(lastNameLine[28 : -7])
-	print("%d,%s %s" % (rank, firstName, lastName))
-	print()
+
+	#get the desired stat
+	desiredStatLine = str(stats[i])
+	stat = str(desiredStatLine[205:-5])
+	print("%d,%s %s, %s" % (rank, firstName, lastName, stat))
+	#print()
 	rank += 1
 
