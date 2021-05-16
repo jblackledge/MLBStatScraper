4b7518b9d3aca55483377b08907074e70c5cb092
Jesse <jesse.m.blackledge-1@ou.edu>
2021-05-16 20:58:58 +0000

    Fixed bug that was causing formatting issues by checking whether current char is a number, tests good

--- MLBWebScraper.py
+++ MLBWebScraper.py
@@ -23,19 +23,32 @@ stats = opsnlSoup.select("td[data-col=\"17\"]")
 
 #loop through list of player name by 2, and parse the html to retrieve a str object for first and last name
 rank = 1;
+statIndex = 0; #needed to access stat line
+nameStartingIndex = 28
+nameEndingIndex = -7
+statEndingIndex = -5
+
 for i in range(0, len(playerNames), 2):
 	#get first and last name of current player as string
 	firstNameLine = str(playerNames[i])
 	lastNameLine = str(playerNames[i + 1])
 
 	#parse string, leaving only the name
-	firstName = str(firstNameLine[28 : -7])
-	lastName = str(lastNameLine[28 : -7])
+	firstName = str(firstNameLine[nameStartingIndex : nameEndingIndex])
+	lastName = str(lastNameLine[nameStartingIndex : nameEndingIndex])
+
 
 	#get the desired stat
-	desiredStatLine = str(stats[i])
-	stat = str(desiredStatLine[205:-5])
-	print("%d,%s %s, %s" % (rank, firstName, lastName, stat))
-	#print()
+	desiredStatLine = str(stats[statIndex])
+	statStartingIndex = 205
+
+	#increment the statStartingIndex if the character at that pos is not a number or a decimal
+	while (desiredStatLine[statStartingIndex] != '.') and (not desiredStatLine[statStartingIndex].isdigit()):
+		statStartingIndex += 1
+	stat = float(desiredStatLine[statStartingIndex : statEndingIndex])
+
+	#print rank, playername, and stat value in csv format and increment values
+	print("%d,%s %s,%.3f" % (rank, firstName, lastName, stat))
 	rank += 1
+	statIndex += 1
 
