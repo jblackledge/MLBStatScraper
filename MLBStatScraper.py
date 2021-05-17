6e1f8ded2cdaf9d3c3a5da261e126a46057b706e
Jesse <jesse.m.blackledge-1@ou.edu>
2021-05-17 04:34:09 +0000

    Fixed issue with rbi function, tests good with the years it was previously failing. More tests to follow.

--- MLBStatScraper.py
+++ MLBStatScraper.py
@@ -1,6 +1,7 @@
 import urllib.request
 from bs4 import BeautifulSoup as soup
 from urllib.request import Request, urlopen
+import datetime
 
 
 def getOPSLeaders(league, statType, year):
@@ -76,11 +77,19 @@ def getOPSLeaders(league, statType, year):
 
 
 
-def getRBILeaders(league, statType):
+
+
+def getRBILeaders(league, year):
+	#dictionary of the stat's column position
 	statDictionary = {"ops" : "17", "avg" : "14", "hr" : "8", "rbi" : "9"}
 	#urls to parse, use request with "User-Agent" header to prevent MLB from blocking bot
-	urlOPS = "https://www.mlb.com/stats/" + league + "-league"
-	request = Request(urlOPS, headers={"User-Agent": "XYZ/3.0"})
+	now = datetime.datetime.now()
+	currentYear = int(now.year)
+	if year == currentYear:
+		url = "https://www.mlb.com/stats/" + league + "-league/rbi"
+	else:
+		url = "https://www.mlb.com/stats/" + league + "-league/rbi/" + year
+	request = Request(url, headers={"User-Agent": "XYZ/3.0"})
 
 	#download webpage, read contents, and close reader
 	downloadedOPSPage = urlopen(request)
@@ -88,24 +97,46 @@ def getRBILeaders(league, statType):
 	downloadedOPSPage.close()
 
 	#create Beautiful Soup object to parse html page contents
-	opsSoup = soup(opsPageHtml, "html.parser")
+	rbiSoup = soup(opsPageHtml, "html.parser")
 
 	#find place in HTML containing player names in the table (ordered based on selected attribute)
-	playerNames = opsSoup.find_all("span", {"class": "full-3fV3c9pF"})
+	playerNames = rbiSoup.find_all("span", {"class": "full-3fV3c9pF"})
 
-	statIndex = 0
 	#find column specific stat
-	stats = opsSoup.select("td[data-col=\"" + statDictionary[statType]  + "\"]")
+	stats = rbiSoup.select("td[data-col=\"9\"]")
+
+
+	#loop through list of player name by 2, and parse the html to retrieve a str object for first and last name
+	rank = 1;
+	statIndex = 0; #needed to access stat line
+	nameStartingIndex = 28
+	nameEndingIndex = -7
+	statEndingIndex = -5
+
 	for i in range(0, len(playerNames), 2):
-		statLine = str(stats[statIndex])
-		startingStatIndex = 185
-		while not statLine[startingStatIndex].isdigit():
-			startingStatIndex += 1
-		rbis = int(statLine[startingStatIndex : -5])
-		print(rbis)
+		#get first and last name of current player as string
+		firstNameLine = str(playerNames[i])
+		lastNameLine = str(playerNames[i + 1])
+
+		#parse string, leaving only the name
+		firstName = str(firstNameLine[nameStartingIndex : nameEndingIndex])
+		lastName = str(lastNameLine[nameStartingIndex : nameEndingIndex])
+
+		#get the desired stat line
+		desiredStatLine = str(stats[statIndex])
+		desiredStatLine = desiredStatLine[0 : -5]
+		index = len(desiredStatLine) - 1
+
+		while desiredStatLine[index].isdigit() and desiredStatLine[index - 1].isdigit():
+			index -= 1
+
+		stat = int(desiredStatLine[index : ])
+		print("%d,%s %s,%d" % (rank, firstName, lastName, stat))
+
+		rank += 1
 		statIndex += 1
 
 
 #getRBILeaders("national", "rbi")
-getOPSLeaders("national", "ops", "1922")
+getRBILeaders("national", "2019")
 
